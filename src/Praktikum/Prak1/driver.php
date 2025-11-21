7e073b7f3d54e050f02a53955feb7fb7a8e16e71
Soukaina Elhafif <soukainaelhafif@MacBook-Air-von-Soukaina.local>
2025-11-22 00:22:13 +0100

    Feature: Lieferstatus-Radiobuttons zur Fahrer-Seite hinzugefügt

--- src/Praktikum/Prak1/driver.php
+++ src/Praktikum/Prak1/driver.php
@@ -3,10 +3,29 @@ $title = "Fahrer";
 require_once("head.php");
 ?>
 <main>
-  <h2>Fahrer-Übersicht</h2>
-  <p>Hier sieht der Fahrer, welche Bestellungen ausgeliefert werden müssen.</p>
+  <h2>Fahrer – Lieferstatus melden</h2>
+
   <form action="echo.php" method="post">
+
+    <p>Lieferstatus auswählen:</p>
+
+    <label>
+      <input type="radio" name="delivery_status" value="unterwegs" required>
+      Unterwegs
+    </label><br>
+
+    <label>
+      <input type="radio" name="delivery_status" value="zugestellt">
+      Zugestellt
+    </label><br>
+
+    <label>
+      <input type="radio" name="delivery_status" value="nicht angetroffen">
+      Nicht angetroffen
+    </label><br><br>
     <input type="submit" value="Lieferung starten">
   </form>
 </main>
 <?php require_once("footer.php"); ?>
+
+
