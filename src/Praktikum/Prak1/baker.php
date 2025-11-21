a6ac82a62a8b51c29482a9d7d023eddc860657d2
Soukaina Elhafif <soukainaelhafif@MacBook-Air-von-Soukaina.local>
2025-11-22 00:22:05 +0100

    Feature: Pizza-Status Radiobuttons zur Bäcker-Seite ergänzt

--- src/Praktikum/Prak1/baker.php
+++ src/Praktikum/Prak1/baker.php
@@ -3,9 +3,26 @@ $title = "Bäcker";
 require_once("head.php");
 ?>
 <main>
-  <h2>Bäcker-Übersicht</h2>
-  <p>Hier sieht der Bäcker, welche Pizzen er backen muss.</p>
+  <h2>Bäcker – Pizza Status bearbeiten</h2>
+
   <form action="echo.php" method="post">
+
+    <p>Pizza Status wählen:</p>
+
+    <label>
+      <input type="radio" name="pizza_status" value="im Ofen" required>
+      Im Ofen
+    </label><br>
+
+    <label>
+      <input type="radio" name="pizza_status" value="fertig">
+      Fertig
+    </label><br>
+
+    <label>
+      <input type="radio" name="pizza_status" value="verbrannt">
+      Verbrannt
+    </label><br><br>
     <input type="submit" value="Daten abrufen">
   </form>
 </main>
